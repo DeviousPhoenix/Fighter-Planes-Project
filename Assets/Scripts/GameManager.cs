679d7a3d8e0a6d01acaf9c5bed88a4a7f88cc631
tkpleo <tkristofferpurdy@gmail.com>
2024-11-02 21:58:49 -0400

    Added second enemy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,14 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
+    public GameObject enemy2;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemy2", 2f, 4f);
     }
 
     // Update is called once per frame
@@ -24,5 +26,9 @@ public class GameManager : MonoBehaviour
     void CreateEnemy()
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 9f, 0), Quaternion.identity);
+    }
+    void CreateEnemy2()
+    {
+        Instantiate(enemy2, new Vector3(Random.Range(-9f, 5f), 9f, 0), Quaternion.identity);
     }
 }
